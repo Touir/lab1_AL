26030ee7ac334eceb5a2ef7b14a5a2c8f007c7e9
Touir <dima.minchenkov.88@mail.ru>
2022-09-10 10:29:42 +0300

    Добавьте файлы проекта.

---
+++ task2/2.cs
@@ -0,0 +1,52 @@
+﻿namespace task2
+{
+
+    public class Rectangle
+    {
+        private double sideA;
+        private double sideB;
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+        
+        double AreaCalculator()
+        {
+            return sideA * sideB;
+        }
+
+        double PerimeterCalculator()
+        {
+            return (sideA + sideB) * 2;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+
+        public double Perimetr
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            double sideA = Convert.ToDouble(Console.ReadLine());
+            double sideB = Convert.ToDouble(Console.ReadLine());
+            var rec = new Rectangle(sideA, sideB);
+            Console.WriteLine("Perimetr of rectangle is {0} \nArea of rectangle is {1}", rec.Perimetr, rec.Area);
+        }
+    }
+}
---
+++ task3/3.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+
+namespace task3 {
+    public class Point { 
+        private double _x;
+        private double _y;
+
+        public Point(double x, double y)
+        {
+            this._x = x; this._y = y;
+        }
+
+        public double x
+        {
+            get {
+                return _x;
+            }
+        }
+        public double y 
+        {
+            get {
+                return _y;
+            }
+        }
+    }
+
+    public class Figure
+    {
+        private Point[] points;
+        public string? name { get; set; }
+
+        public Figure(int n, string name)
+        {
+            this.name = name;   
+            points = new Point[n];
+            if (n >= 3 || n <=5)
+            {
+                for (int i = 0; i < n; ++i)
+                {
+                    Console.WriteLine("Point {0}:", i+1);
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    points[i] = new Point(x, y);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid number of points!!! \n");
+            }
+        }
+
+        double LengthSide(Point A, Point B) 
+        {
+            double length = Math.Sqrt((B.x - A.x)*(B.x - A.x) + (B.y - A.y)*(B.y - A.y));
+            return length;
+        }
+
+        double PerimeterCalculator() 
+        {
+            double perimetr = 0;
+            for (int i = 0; i < points.Length - 1; ++i) 
+            {
+                perimetr += LengthSide(points[i], points[i + 1]);
+            }
+            perimetr += LengthSide(points[points.Length - 1], points[0]);
+            return perimetr;
+        }
+
+        public double Perimetr
+        {
+            get 
+            { 
+                return PerimeterCalculator();
+            }
+        }
+    }
+
+    public class Program {
+        static void Main() {
+            Console.WriteLine("Quantity of points in figure :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name of figure:");
+            string? name = Console.ReadLine();
+            var fig = new Figure(n, name);
+            Console.WriteLine("Perimetr of figure {0} is {1}",fig.name, fig.Perimetr);
+        }
+    }
+
+}
